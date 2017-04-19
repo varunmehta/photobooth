87e1164f8252430fce3b35a54c34b44a177808ad
Varun Mehta <varun.mehta.r@gmail.com>
2017-04-19 00:44:52 -0400

    #5 Added slash in end

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 monitor_w = 800  # width of the display monitor
 monitor_h = 400  # height of the display monitor
 event_name = 'international_night'
-file_path = '/home/pi/photobooth/' + event_name  # path to save images
+file_path = '/home/pi/photobooth/' + event_name + "/"  # path to save images
 clear_on_startup = False  # True will clear previously stored photos as the program launches. False will leave all previous photos.
 debounce = 0.3  # how long to debounce the button. Add more time if the button triggers too many times.
 post_online = True  # True to upload images. False to store locally only.
