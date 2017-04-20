18c10aef3e3790182634856a7382cad95b072fc2
Varun Mehta <varun.mehta.r@gmail.com>
2017-04-20 14:29:24 -0400

    #7 back to 10 seconds.

--- start-photobooth.sh
+++ start-photobooth.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 # sleep for 3 seconds before starting again
-sleep 3
+sleep 10
 cd /opt/photobooth
 python3 photobooth.py
