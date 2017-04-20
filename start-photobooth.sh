60f98ea15aa9254905973a5ed6afcf2045c69b3e
Varun Mehta <varun.mehta.r@gmail.com>
2017-04-20 12:44:22 -0400

    #7, added sleep to keep a delay on startup

--- start-photobooth.sh
+++ start-photobooth.sh
@@ -2,5 +2,7 @@
 # start-photobooth.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
+# sleep for 3 seconds before starting again
+sleep 3
 cd /opt/photobooth
 python3 photobooth.py
