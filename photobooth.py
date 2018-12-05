ae303bf55978b50a874d362c196c73cb2aeb95e2
Varun Mehta <varun.mehta.r@gmail.com>
2018-12-04 23:10:29 -0500

    pyramidci

--- photobooth.py
+++ photobooth.py
@@ -275,7 +275,7 @@ def start_photobooth():
 
     # Create a montage of the images
     montage = "gm montage -mode concatenate -resize 1190x1770 -borderwidth 5 -bordercolor white " \
-              + config.file_path + "/" + now + "-01.jpg  " + real_path + "/smita-40-middle.jpg " \
+              + config.file_path + "/" + now + "-01.jpg  " + real_path + "/pyramidci-middle.jpg " \
               + config.file_path + "/" + now + "-02.jpg -tile 1x3 " \
               + config.file_path + "/final/" + montage_img
 
