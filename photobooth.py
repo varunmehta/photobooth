9dbb0c3c78e674d21e4ed9d3dea0e9777fbbbc51
Varun Mehta <varun.mehta.r@gmail.com>
2019-04-09 17:06:45 -0400

    international night 2019

--- photobooth.py
+++ photobooth.py
@@ -275,7 +275,7 @@ def start_photobooth():
 
     # Create a montage of the images
     montage = "gm montage -mode concatenate -resize 1190x1770 -borderwidth 5 -bordercolor white " \
-              + config.file_path + "/" + now + "-01.jpg  " + real_path + "/stem-middle.jpg " \
+              + config.file_path + "/" + now + "-01.jpg  " + real_path + "/international-middle.jpg " \
               + config.file_path + "/" + now + "-02.jpg -tile 1x3 " \
               + config.file_path + "/final/" + montage_img
 
