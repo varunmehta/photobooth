8cb15e3d8d136fe3037db7f13ed35456abf048dc
Varun Mehta <varun.mehta.r@gmail.com>
2019-10-23 23:54:20 -0400

    diwali 2019

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Config settings to change behavior of photo booth
 monitor_w = 800  # width of the display monitor
 monitor_h = 480  # height of the display monitor
-event_name = 'international'
+event_name = 'diwali2019'
 file_path = '/home/pi/photobooth/' + event_name + '/'  # path to save images
 clear_on_startup = False  # True will clear previously stored photos as the program launches. False will leave all previous photos.
 debounce = 0.3  # how long to debounce the button. Add more time if the button triggers too many times.
@@ -12,5 +12,5 @@ hi_res_pics = True  # True to save high res pics from camera.
 # If also uploading, the program will also convert each image to a smaller image before making the gif.
 # False to first capture low res pics. False is faster.
 # Careful, each photo costs against your daily Tumblr upload max.
-camera_iso = 800  # adjust for lighting issues. Normal is 100 or 200. Sort of dark is 400. Dark is 800 max.
+camera_iso = 200  # adjust for lighting issues. Normal is 100 or 200. Sort of dark is 400. Dark is 800 max.
 # available options: 100, 200, 320, 400, 500, 640, 800
